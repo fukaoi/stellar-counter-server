23bc979ea45cf6d8695ba308d8c234c54285bf1b
fukaoi <goodakira7@gmail.com>
2018-11-29 19:06:12 +0900

    modified

--- app.js
+++ app.js
@@ -2,27 +2,26 @@ const render = require('./lib/render');
 const logger = require('koa-logger');
 const router = require('koa-router')();
 const koaBody = require('koa-body');
+const serve = require('koa-static');
 
 const Koa = require('koa');
 const app = module.exports = new Koa();
 
-
-const posts = [];
+const lists = [];
 
 app.use(logger());
 app.use(render);
 app.use(koaBody());
-
 router.get('/lumen', lumen).get('/token', token);
-
 app.use(router.routes());
+app.use(serve('./public'));
 
 async function lumen(ctx) {
-  await ctx.render('lumen', {posts: posts});
+  await ctx.render('lumen', {lists: lists});
 }
 
 async function token(ctx) {
-  await ctx.render('token', {posts: posts});
+  await ctx.render('token', {lists: lists});
 }
 
 
