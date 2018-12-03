b8e245eed3f68ed5529fceaa78d2a57666bd1c30
fukaoi <goodakira7@gmail.com>
2018-12-03 17:34:30 +0900

    modified

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const render = require('./lib/render');
+const ClientSdk = require('./lib/stellar-client');
 const logger = require('koa-logger');
 const router = require('koa-router')();
 const koaBody = require('koa-body');
@@ -16,14 +17,21 @@ router.get('/lumen', lumen).get('/token', token);
 app.use(router.routes());
 
 async function lumen(ctx) {
-  let lists = 
-    [
-    { address: "xxxxxxxxxxxxxx", timestamp: "20101-101001-11", amount: 10 },
-    { address: "xxxxxxxxxx3333333", timestamp: "2018-101001-11", amount: 30 }, { address: "44444444444", timestamp: "2019-101001-11", amount: 40 }
-    ]
+  // let lists = 
+  //   [
+  //   { address: "xxxxxxxxxxxxxx", timestamp: "20101-101001-11", amount: 10 },
+  //   { address: "xxxxxxxxxx3333333", timestamp: "2018-101001-11", amount: 30 }, { address: "44444444444", timestamp: "2019-101001-11", amount: 40 }
+  //   ]
 
-  console.log(lists);
-  await ctx.render('lumen', {lists: lists});
+  let lists = [];
+  const client = new ClientSdk();
+  let store_tx = async (address, amount, timestamp) => {
+    let obj = { address: address, amount: amount, timestamp: timestamp };
+    lists.push(obj);
+    console.log(lists.length);
+  }
+  client.paymentMessage(store_tx);
+  await ctx.render('lumen', { lists: lists });
 }
 
 async function token(ctx) {
