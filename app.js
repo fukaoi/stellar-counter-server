4659b70318ca634317ab9e423c7d43eb87a6746e
fukaoi <goodakira7@gmail.com>
2018-12-07 19:43:34 +0900

    WIP

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const StellarClient = require('./lib/stellar-client');
 const logger = require('koa-logger');
 const router = require('koa-router')();
 const koaBody = require('koa-body');
-const crypto = require('crypto');
 
 const Koa = require('koa');
 const app = module.exports = new Koa();
@@ -14,7 +13,8 @@ let lists = [];
 app.use(logger());
 app.use(render);
 app.use(koaBody());
-router.get('/lumen', lumen).get('/token', token).post('/send', send);
+router.get('/lumen', lumen).get('/token', token).get('/decode', decode)
+      .post('/send', send);
 app.use(router.routes());
 
 async function lumen(ctx) {
@@ -32,6 +32,13 @@ async function send(ctx) {
   const param = ctx.request.body;
   new StellarClient().sendXLM(param.publickey, param.amount, param.txhash);
   ctx.response.status = 201;
+}
+
+async function decode(ctx) {
+  const param = ctx.request.body;
+  console.log(param);
+  const buffer = Buffer.from(param.encoded_memo, 'base64');
+  ctx.response.body = buffer.toString('hex');
 }
 
 if (!module.parent) app.listen(3000);
