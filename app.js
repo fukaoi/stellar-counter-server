9e925753ba2b1fa17de52875132bef226b02412a
fukaoi <goodakira7@gmail.com>
2018-12-12 12:58:31 +0900

    Refactoring

--- app.js
+++ app.js
@@ -1,55 +1,37 @@
-const render = require('./lib/render');
-const config = require('./config/config.json');
-const StellarClient = require('./lib/stellar-client');
-const logger = require('koa-logger');
-const router = require('koa-router')();
-const koaBody = require('koa-body');
+const render = require('./lib/render')
+const config = require('./config/config.json')
+const StellarClient = require('./lib/stellar-client')
+const logger = require('koa-logger')
+const router = require('koa-router')()
+const koaBody = require('koa-body')
 
-const Koa = require('koa');
-const app = module.exports = new Koa();
-const sse = require('koa-sse-stream');
+const Koa = require('koa')
+const app = module.exports = new Koa()
 
-app.use(logger());
-app.use(render);
-app.use(koaBody());
-app.use(router.routes());
+app.use(logger())
+app.use(render)
+app.use(koaBody())
+app.use(router.routes())
 
-router.get('/lumen', lumen).get('/token', token).get('/decode', decode)
-  .post('/send', send);
-
-app.use(sse({
-  maxClients: 50,
-  pingInterval: 1000
-}));
-
-app.use(async (ctx) => {
-  new StellarClient().paymentMessage(ctx.sse.send);
-  ctx.sse.end();
-});
+router.get('/lumen', lumen).get('/token', token)
+  .post('/send', send)
 
 async function lumen(ctx) {
   await ctx.render('lumen', {
     horizonUrl: config['horizonUrl'],
     publicKey: config['publicKey'],
     limit: config['limit']
-  });
+  })
 }
 
 async function token(ctx) {
-  await ctx.render('token', { lists: lists });
+  await ctx.render('token', { lists: lists })
 }
 
 async function send(ctx) {
-  const param = ctx.request.body;
-  new StellarClient().sendXLM(param.publickey, param.amount, param.txhash);
-  ctx.response.status = 201;
+  const param = ctx.request.body
+  new StellarClient().sendXLM(param.publickey, param.amount, param.txhash)
+  ctx.response.status = 201
 }
 
-async function decode(ctx) {
-  const param = ctx.request.body;
-  console.log(param);
-  const buffer = Buffer.from(param.encoded_memo, 'base64');
-  ctx.response.body = buffer.toString('hex');
-}
-
-if (!module.parent) app.listen(3000);
+if (!module.parent) app.listen(3000)
